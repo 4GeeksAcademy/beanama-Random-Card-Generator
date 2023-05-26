07a7a0e41e0d4c47790710f96ffb455e21c638f6
beanama <129529299+beanama@users.noreply.github.com>
2023-05-26 19:35:34 +0000

    card generated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let symbol = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const selectedNumber = getRandomCharacter(number);
+  const selectedSymbol = getRandomCharacter(symbol);
+
+  function generateCard() {
+    document.getElementById("randomCard").innerHTML = `<div class="row">
+  
+  <div id="simbolo1" class="col-3 symbol align-self-start">
+  ${selectedSymbol}
+  </div>
+  <div class="col-6 number align-self-center">
+  ${selectedNumber}
+  </div>
+  <div id="simbolo2" class="col-3 symbol align-self-end">
+  ${selectedSymbol}
+  </div>
+  
+  </div>`;
+  }
+
+  generateCard();
+
+  if (selectedSymbol === "♥" || selectedSymbol === "♦") {
+    const symbolElements = document.querySelectorAll(".symbol");
+    symbolElements.forEach(function(element) {
+      element.style.color = "red";
+    });
+  }
 };
+
+function getRandomCharacter(array) {
+  const index = Math.floor(Math.random() * array.length);
+  const selectedItem = array[index];
+  return selectedItem;
+}

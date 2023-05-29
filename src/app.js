ba856b8ce65c9d43ef6f242203823343d37a0b40
beanama <129529299+beanama@users.noreply.github.com>
2023-05-29 19:19:49 +0000

    button and timer added

--- src/app.js
+++ src/app.js
@@ -9,34 +9,55 @@ let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
 let symbol = ["♦", "♥", "♠", "♣"];
 
 window.onload = function() {
-  const selectedNumber = getRandomCharacter(number);
-  const selectedSymbol = getRandomCharacter(symbol);
-
   function generateCard() {
-    document.getElementById("randomCard").innerHTML = `<div class="row">
-  
-  <div id="simbolo1" class="col-3 symbol align-self-start">
-  ${selectedSymbol}
-  </div>
-  <div class="col-6 number align-self-center">
-  ${selectedNumber}
-  </div>
-  <div id="simbolo2" class="col-3 symbol align-self-end">
-  ${selectedSymbol}
-  </div>
-  
-  </div>`;
+    const selectedNumber = getRandomCharacter(number);
+    const selectedSymbol = getRandomCharacter(symbol);
+
+    document.getElementById("simbolo1").innerHTML = `${selectedSymbol}`;
+    document.getElementById("numero").innerHTML = `${selectedNumber}`;
+    document.getElementById("simbolo2").innerHTML = `${selectedSymbol}`;
+
+    if (selectedSymbol === "♥" || selectedSymbol === "♦") {
+      const symbolElements = document.querySelectorAll(".symbol");
+      symbolElements.forEach(function(element) {
+        element.style.color = "red";
+      });
+    }
   }
 
   generateCard();
 
-  if (selectedSymbol === "♥" || selectedSymbol === "♦") {
-    const symbolElements = document.querySelectorAll(".symbol");
-    symbolElements.forEach(function(element) {
-      element.style.color = "red";
-    });
-  }
+  //Botón para generar carta
+
+  const createCardButton = document.getElementById("new-card");
+
+  createCardButton.addEventListener("click", function() {
+    generateCard();
+  });
+
+  //Elegir ancho y alto
+
+  const widthInput = document.getElementById("card-width");
+  const heightInput = document.getElementById("card-height");
+  const card = document.getElementById("randomCard");
+
+  widthInput.addEventListener("input", e => {
+    card.style.width = e.target.value + "px";
+  });
+
+  heightInput.addEventListener("input", e => {
+    card.style.height = e.target.value + "px";
+  });
+
+  //Set Timer
+
+  setInterval(() => {
+    console.log("hola");
+    generateCard();
+  }, 1000);
 };
+
+//Definición de funciones
 
 function getRandomCharacter(array) {
   const index = Math.floor(Math.random() * array.length);
